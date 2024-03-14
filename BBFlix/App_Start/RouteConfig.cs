8172578c3765e2fb13067035ed3f59633925f94a
Khanh <dokimkhanh0709@gmail.com>
2024-03-14 16:35:42 +0700

    update frontend theme

--- BBFlix/App_Start/RouteConfig.cs
+++ BBFlix/App_Start/RouteConfig.cs
@@ -14,9 +14,52 @@ namespace BBFlix
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "Contact",
+               url: "lien-he",
+               defaults: new { controller = "Page", action = "Contact", id = UrlParameter.Optional },
+               namespaces: new[] { "BBFlix.Controllers" }
+           );
+
+            routes.MapRoute(
+               name: "FAQs",
+               url: "faqs",
+               defaults: new { controller = "Page", action = "FAQs", id = UrlParameter.Optional },
+               namespaces: new[] { "BBFlix.Controllers" }
+           );
+
+            routes.MapRoute(
+               name: "Upgrade",
+               url: "nang-cap",
+               defaults: new { controller = "Page", action = "AccoutUpgrade", id = UrlParameter.Optional },
+               namespaces: new[] { "BBFlix.Controllers" }
+           );
+
+            routes.MapRoute(
+                name: "About",
+                url: "gioi-thieu",
+                defaults: new { controller = "Page", action = "About", id = UrlParameter.Optional },
+                namespaces: new[] { "BBFlix.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "SingUp",
+                url: "dang-ky",
+                defaults: new { controller = "Signup", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "BBFlix.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "dang-nhap",
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "BBFlix.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "BBFlix.Controllers" }
             );
         }
     }
---
+++ BBFlix/Controllers/PageController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBFlix.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult AccoutUpgrade()
+        {
+            return View();
+        }
+
+        public ActionResult FAQs()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
