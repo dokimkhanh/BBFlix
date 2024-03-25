3fa6f56274ef00d8ac11eb4c437d4e12b24bc102
Khanh <dokimkhanh0709@gmail.com>
2024-03-25 16:42:22 +0700

    update dasboard admin

--- BBFlix/Areas/Admin/Controllers/HomeController.cs
+++ BBFlix/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BBFlix.Models.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,20 @@ namespace BBFlix.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+
+        private BBFlixContext _context;
+
+        public HomeController()
+        {
+            _context = new BBFlixContext();
+        }
+
         // GET: Admin/Home
         public ActionResult Index()
         {
+            ViewBag.MovieMonth = _context.Movie.Where(x => x.mov_date_rel.Month == DateTime.Now.Month).ToList();
+            ViewBag.NewComments = _context.Reviewer.Where(x => x.rev_date.Month == DateTime.Now.Month).ToList();
+            ViewBag.NewRate = _context.Reviewer.Where(x => x.rev_date.Month == DateTime.Now.Month && x.rev_star > 0).ToList();
             return View();
         }
     }
